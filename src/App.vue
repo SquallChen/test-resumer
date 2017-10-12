fd3bf229ad4371891ca4e41b994a0ce3c4b8d771
MR.C <442947365@qq.com>
2017-10-12 18:26:06 +0800

    完成tab切换和resumeEditor页面的基本架构

--- src/App.vue
+++ src/App.vue
@@ -16,38 +16,42 @@ import './assets/reset.css'
 import Topbar from './components/Topbar'
 import ResumeEditor from './components/ResumeEditor'
 import ResumePreview from './components/ResumePreview'
+import icons from './assets/icons'
 
 export default {
-  name: 'app',
-  data: function(){
-    return {
-      text: '你好'
+     name: 'app',
+     data: function () {
+       return {
+         text: '你好'
+       }
+     },
+     components: { Topbar, ResumeEditor, ResumePreview },
+     created() {
+       document.body.insertAdjacentHTML('afterbegin', icons) //
     }
-  },
-  components: {Topbar, ResumeEditor, ResumePreview}
-}
+   }
 </script>
 
-<style>
+<style lang="scss">
 .page{
     height: 100vh;
     display: flex;
     flex-direction: column;
     background: #EAEBEC;
-  }
-  .page>main{
-    flex-grow: 1;  
-  }
-  .page>main{
-    min-width: 1024px;
-    max-width: 1440px;
-    margin-top: 16px;
-    margin-bottom: 16px;
-    display: flex;
-    justify-content: space-between;
-    padding: 0 16px;
-    width: 100%; 
-   align-self: center;
+    >main{
+      flex-grow: 1;  
+    }
+    >main{
+      min-width: 1024px;
+      max-width: 1440px;
+      margin-top: 16px;
+      margin-bottom: 16px;
+      display: flex;
+      justify-content: space-between;
+      padding: 0 16px;
+      width: 100%; /* 试试不加这句会怎样 */
+      align-self: center;
+    }
   }
   #resumeEditor{
     width: 35%;
@@ -58,4 +62,11 @@ export default {
     margin-left: 16px;
     background: #777;
   }
+  svg.icon{
+     height: 1em;
+     width: 1em;
+     fill: currentColor;
+     vertical-align: -0.1em;
+     font-size:16px;
+   }
 </style>
